bfc1220d14dafd96532dc1008e0df6c6b50042be
Azriel Hoh <azriel91@gmail.com>
2026-01-02 13:44:16 +1300

    Add `NodeContentSpansCalculator`.

--- crate/input_ir_rt/src/lib.rs
+++ crate/input_ir_rt/src/lib.rs
@@ -2,7 +2,9 @@
 
 pub use crate::{
     input_to_ir_diagram_mapper::InputToIrDiagramMapper, ir_to_taffy_builder::IrToTaffyBuilder,
+    node_content_spans_calculator::NodeContentSpansCalculator,
 };
 
 mod input_to_ir_diagram_mapper;
 mod ir_to_taffy_builder;
+mod node_content_spans_calculator;
---
+++ crate/input_ir_rt/src/node_content_spans_calculator.rs
@@ -0,0 +1,10 @@
+use disposition_ir_model::IrDiagram;
+
+#[derive(Clone, Copy, Debug)]
+pub struct NodeContentSpansCalculator;
+
+impl NodeContentSpansCalculator {
+    pub fn map(_ir_diagram: &IrDiagram) -> NodeContentSpans {
+        todo!()
+    }
+}
