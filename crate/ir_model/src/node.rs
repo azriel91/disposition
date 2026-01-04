45a80ad3f5b9a06d95432e32b56c3926f3713c51
Azriel Hoh <azriel91@gmail.com>
2026-01-05 10:19:56 +1300

    Revert "Add `NodeContentSpansCalculator`."
    This reverts commit bfc1220d14dafd96532dc1008e0df6c6b50042be.

--- crate/ir_model/src/node.rs
+++ crate/ir_model/src/node.rs
@@ -1,10 +1,8 @@
 pub use self::{
-    node_content_span::NodeContentSpan, node_copy_text::NodeCopyText,
-    node_hierarchy::NodeHierarchy, node_id::NodeId, node_inbuilt::NodeInbuilt,
-    node_names::NodeNames,
+    node_copy_text::NodeCopyText, node_hierarchy::NodeHierarchy, node_id::NodeId,
+    node_inbuilt::NodeInbuilt, node_names::NodeNames,
 };
 
-mod node_content_span;
 mod node_copy_text;
 mod node_hierarchy;
 mod node_id;
