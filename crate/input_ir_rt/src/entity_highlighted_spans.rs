8da264061957002c87d4c10df9e48e596ec4af59
Azriel Hoh <azriel91@gmail.com>
2026-01-05 10:35:55 +1300

    Add `EntityHighlightedSpans` map.

---
+++ crate/input_ir_rt/src/entity_highlighted_spans.rs
@@ -0,0 +1,67 @@
+use std::ops::{Deref, DerefMut};
+
+use disposition_model_common::{Id, Map};
+use serde::{Deserialize, Serialize};
+use syntect::highlighting::Style;
+
+/// `syntect` highlighted spans, empty
+#[derive(Clone, Debug, Default, PartialEq, Eq, Deserialize, Serialize)]
+pub struct EntityHighlightedSpans(Map<Id, Vec<(Style, String)>>);
+
+impl EntityHighlightedSpans {
+    /// Returns a new `EntityHighlightedSpans` map.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns a new `EntityHighlightedSpans` map with the given preallocated
+    /// capacity.
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self(Map::with_capacity(capacity))
+    }
+
+    /// Returns the underlying map.
+    pub fn into_inner(self) -> Map<Id, Vec<(Style, String)>> {
+        self.0
+    }
+
+    /// Returns true if the map is empty.
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Returns true if this contains highlighted spans for an entity with the
+    /// given ID.
+    pub fn contains_key<IdT>(&self, id: &IdT) -> bool
+    where
+        IdT: AsRef<Id>,
+    {
+        self.0.contains_key(id.as_ref())
+    }
+}
+
+impl Deref for EntityHighlightedSpans {
+    type Target = Map<Id, Vec<(Style, String)>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for EntityHighlightedSpans {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl From<Map<Id, Vec<(Style, String)>>> for EntityHighlightedSpans {
+    fn from(inner: Map<Id, Vec<(Style, String)>>) -> Self {
+        Self(inner)
+    }
+}
+
+impl FromIterator<(Id, Vec<(Style, String)>)> for EntityHighlightedSpans {
+    fn from_iter<I: IntoIterator<Item = (Id, Vec<(Style, String)>)>>(iter: I) -> Self {
+        Self(Map::from_iter(iter))
+    }
+}
