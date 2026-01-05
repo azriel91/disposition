2fb0eb4e96af6b1ac39e097ea6251e679af4dece
Azriel Hoh <azriel91@gmail.com>
2026-01-06 11:24:03 +1300

    Use cumulative x and y coordinates for SVG node placement.

--- crate/input_ir_rt/src/taffy_to_svg_mapper.rs
+++ crate/input_ir_rt/src/taffy_to_svg_mapper.rs
@@ -112,8 +112,20 @@ impl TaffyToSvgMapper {
                 return;
             };
 
-            let x = layout.location.x;
-            let y = layout.location.y;
+            let (x, y) = {
+                let mut x_acc = layout.location.x;
+                let mut y_acc = layout.location.y;
+                let mut current_node_id = taffy_node_id;
+                while let Some(parent_taffy_node_id) = taffy_tree.parent(current_node_id) {
+                    let Ok(parent_layout) = taffy_tree.layout(parent_taffy_node_id) else {
+                        break;
+                    };
+                    x_acc += parent_layout.location.x;
+                    y_acc += parent_layout.location.y;
+                    current_node_id = parent_taffy_node_id;
+                }
+                (x_acc, y_acc)
+            };
             let width = layout.size.width;
             let height = layout.size.height;
 
