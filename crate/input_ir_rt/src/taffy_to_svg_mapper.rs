9da7a4bc4ebc01c4f16d87683c6f48d4e670acb1
Azriel Hoh <azriel91@gmail.com>
2026-01-06 11:40:37 +1300

    Set `<text>` `stroke-width="0"`.

--- crate/input_ir_rt/src/taffy_to_svg_mapper.rs
+++ crate/input_ir_rt/src/taffy_to_svg_mapper.rs
@@ -103,92 +103,102 @@ impl TaffyToSvgMapper {
         buffer: &mut String,
         styles_buffer: &mut String,
     ) {
-        ir_diagram.node_ordering.iter().for_each(|(node_id, &tab_index)| {
-            // Look up taffy layout for this node
-            let Some(&taffy_node_id) = node_id_to_taffy.get(node_id) else {
-                return;
-            };
-            let Ok(layout) = taffy_tree.layout(taffy_node_id) else {
-                return;
-            };
+        ir_diagram
+            .node_ordering
+            .iter()
+            .for_each(|(node_id, &tab_index)| {
+                // Look up taffy layout for this node
+                let Some(&taffy_node_id) = node_id_to_taffy.get(node_id) else {
+                    return;
+                };
+                let Ok(layout) = taffy_tree.layout(taffy_node_id) else {
+                    return;
+                };
 
-            let (x, y) = {
-                let mut x_acc = layout.location.x;
-                let mut y_acc = layout.location.y;
-                let mut current_node_id = taffy_node_id;
-                while let Some(parent_taffy_node_id) = taffy_tree.parent(current_node_id) {
-                    let Ok(parent_layout) = taffy_tree.layout(parent_taffy_node_id) else {
-                        break;
-                    };
-                    x_acc += parent_layout.location.x;
-                    y_acc += parent_layout.location.y;
-                    current_node_id = parent_taffy_node_id;
+                let (x, y) = {
+                    let mut x_acc = layout.location.x;
+                    let mut y_acc = layout.location.y;
+                    let mut current_node_id = taffy_node_id;
+                    while let Some(parent_taffy_node_id) = taffy_tree.parent(current_node_id) {
+                        let Ok(parent_layout) = taffy_tree.layout(parent_taffy_node_id) else {
+                            break;
+                        };
+                        x_acc += parent_layout.location.x;
+                        y_acc += parent_layout.location.y;
+                        current_node_id = parent_taffy_node_id;
+                    }
+                    (x_acc, y_acc)
+                };
+                let width = layout.size.width;
+                let height = layout.size.height;
+
+                let node_id_str = node_id.as_str();
+
+                // Build class attribute if tailwind classes exist
+                let class_attr = ir_diagram
+                    .tailwind_classes
+                    .get(node_id.as_ref())
+                    .map(|classes| {
+                        let mut classes_str = String::with_capacity(classes.len() + 25);
+                        classes_str.push_str(r#" class=""#);
+                        classes.chars().for_each(|c| {
+                            if c == '&' {
+                                classes_str.push_str("&amp;");
+                            } else {
+                                classes_str.push(c);
+                            }
+                        });
+                        classes_str.push_str("\"");
+                        classes_str
+                    })
+                    .unwrap_or_default();
+
+                // Start group element with id, tabindex, and optional class
+                write!(
+                    buffer,
+                    r#"<g id="{node_id_str}"{class_attr} tabindex="{tab_index}">"#
+                )
+                .unwrap();
+
+                // Add transform style for positioning
+                writeln!(
+                    styles_buffer,
+                    "#{node_id_str} {{ transform: translate({x}px, {y}px); }}"
+                )
+                .unwrap();
+
+                // Add rect element
+                write!(buffer, r#"<rect width="{width}" height="{height}"/>"#).unwrap();
+
+                // Add text elements for highlighted spans if they exist
+                if let Some(spans) = entity_highlighted_spans.get(node_id.as_ref()) {
+                    for span in spans {
+                        let text_x = span.x;
+                        let text_y = span.y;
+                        let r = span.style.foreground.r;
+                        let g = span.style.foreground.g;
+                        let b = span.style.foreground.b;
+                        let fill_color = format!("#{r:02x}{g:02x}{b:02x}");
+                        let text_content = Self::escape_xml(&span.text);
+
+                        // zero stroke-width because we want the tailwind classes from `<g>` to
+                        // apply to the `<rect>`, but not to the `<text>`
+                        write!(
+                            buffer,
+                            "<text \
+                                x=\"{text_x}\" \
+                                y=\"{text_y}\" \
+                                fill=\"{fill_color}\" \
+                                stroke-width=\"0\" \
+                            >{text_content}</text>"
+                        )
+                        .unwrap();
+                    }
                 }
-                (x_acc, y_acc)
-            };
-            let width = layout.size.width;
-            let height = layout.size.height;
 
-            let node_id_str = node_id.as_str();
-
-            // Build class attribute if tailwind classes exist
-            let class_attr = ir_diagram
-                .tailwind_classes
-                .get(node_id.as_ref())
-                .map(|classes| {
-                    let mut classes_str = String::with_capacity(classes.len() + 25);
-                    classes_str.push_str(r#" class=""#);
-                    classes.chars().for_each(|c| {
-                        if c == '&' {
-                            classes_str.push_str("&amp;");
-                        } else {
-                            classes_str.push(c);
-                        }
-                    });
-                    classes_str.push_str("\"");
-                    classes_str
-                })
-                .unwrap_or_default();
-
-            // Start group element with id, tabindex, and optional class
-            write!(
-                buffer,
-                r#"<g id="{node_id_str}"{class_attr} tabindex="{tab_index}">"#
-            )
-            .unwrap();
-
-            // Add transform style for positioning
-            writeln!(
-                styles_buffer,
-                "#{node_id_str} {{ transform: translate({x}px, {y}px); }}"
-            )
-            .unwrap();
-
-            // Add rect element
-            write!(buffer, r#"<rect width="{width}" height="{height}"/>"#).unwrap();
-
-            // Add text elements for highlighted spans if they exist
-            if let Some(spans) = entity_highlighted_spans.get(node_id.as_ref()) {
-                for span in spans {
-                    let text_x = span.x;
-                    let text_y = span.y;
-                    let r = span.style.foreground.r;
-                    let g = span.style.foreground.g;
-                    let b = span.style.foreground.b;
-                    let fill_color = format!("#{r:02x}{g:02x}{b:02x}");
-                    let text_content = Self::escape_xml(&span.text);
-
-                    write!(
-                        buffer,
-                        r#"<text x="{text_x}" y="{text_y}" fill="{fill_color}">{text_content}</text>"#
-                    )
-                    .unwrap();
-                }
-            }
-
-            // Close group element
-            buffer.push_str("</g>");
-        });
+                // Close group element
+                buffer.push_str("</g>");
+            });
     }
 
     /// Escape XML special characters in text content
