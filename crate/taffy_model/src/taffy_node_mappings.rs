199a9246755b4eb9304c2d00e596fd91a8d9bbcb
Azriel Hoh <azriel91@gmail.com>
2026-03-14 22:57:30 +1300

    Determine equality of taffy nodes using recursive check of node IDs and layout.

--- crate/taffy_model/src/taffy_node_mappings.rs
+++ crate/taffy_model/src/taffy_node_mappings.rs
@@ -28,17 +28,49 @@ impl<'id> PartialEq for TaffyNodeMappings<'id> {
     fn eq(&self, other: &Self) -> bool {
         let self_root = self.node_inbuilt_to_taffy.get(&NodeInbuilt::Root).copied();
         let other_root = other.node_inbuilt_to_taffy.get(&NodeInbuilt::Root).copied();
-        self_root == other_root
-            && self_root
-                .zip(other_root)
-                .map(|(self_root, other_root)| {
-                    self.taffy_tree.children(self_root).ok()
-                        == other.taffy_tree.children(other_root).ok()
-                })
-                .unwrap_or_default()
+
+        let self_taffy_tree = &self.taffy_tree;
+        let other_taffy_tree = &other.taffy_tree;
+        taffy_nodes_eq(self_taffy_tree, self_root, other_taffy_tree, other_root)
             && self.node_inbuilt_to_taffy == other.node_inbuilt_to_taffy
             && self.node_id_to_taffy == other.node_id_to_taffy
             && self.taffy_id_to_node == other.taffy_id_to_node
             && self.entity_highlighted_spans == other.entity_highlighted_spans
     }
+}
+
+fn taffy_nodes_eq(
+    self_taffy_tree: &TaffyTree<NodeContext>,
+    self_root: Option<taffy::NodeId>,
+    other_taffy_tree: &TaffyTree<NodeContext>,
+    other_root: Option<taffy::NodeId>,
+) -> bool {
+    self_root == other_root
+        && self_root
+            .zip(other_root)
+            .map(|(self_root, other_root)| {
+                let self_children = self_taffy_tree.children(self_root).ok();
+                let other_children = other_taffy_tree.children(other_root).ok();
+
+                match (self_children, other_children) {
+                    (None, None) => true,
+                    (None, Some(_)) | (Some(_), None) => false,
+                    (Some(self_children), Some(other_children)) => {
+                        self_children.len() == other_children.len()
+                            && self_children.iter().copied().zip(other_children).all(
+                                |(self_child, other_child)| {
+                                    self_taffy_tree.layout(self_child).ok()
+                                        == other_taffy_tree.layout(other_child).ok()
+                                        && taffy_nodes_eq(
+                                            self_taffy_tree,
+                                            Some(self_child),
+                                            other_taffy_tree,
+                                            Some(other_child),
+                                        )
+                                },
+                            )
+                    }
+                }
+            })
+            .unwrap_or_default()
 }
