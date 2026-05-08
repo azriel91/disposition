436bf3ce9af512ce7b169e3b52bfd7012853dd9f
Azriel Hoh <azriel91@gmail.com>
2026-05-08 16:27:24 +1200

    Update `CHANGELOG.md`.

--- crate/input_ir_rt/src/taffy_to_svg_elements_mapper/ortho_protrusion_calculator.rs
+++ crate/input_ir_rt/src/taffy_to_svg_elements_mapper/ortho_protrusion_calculator.rs
@@ -1617,21 +1617,19 @@ impl OrthoProtrusionCalculator {
                     .map(|spacers| !spacers.is_empty())
                     .unwrap_or(false);
 
-                if !edge_has_spacers {
-                    if let Some(to_face) = pass1_info.to_face {
-                        let min_to = Self::min_protrusion_divergent_sibling_extent(
-                            &pass1_info.edge.to,
-                            &pass1_info.edge.from,
-                            to_face,
-                            node_nesting_infos,
-                            node_ranks_nested,
-                            svg_node_info_map,
-                            entity_types,
-                        );
-                        if min_to > 0.0 {
-                            let params = &mut result[group_idx][edge_idx];
-                            params.to_protrusion = params.to_protrusion.max(min_to);
-                        }
+                if !edge_has_spacers && let Some(to_face) = pass1_info.to_face {
+                    let min_to = Self::min_protrusion_divergent_sibling_extent(
+                        &pass1_info.edge.to,
+                        &pass1_info.edge.from,
+                        to_face,
+                        node_nesting_infos,
+                        node_ranks_nested,
+                        svg_node_info_map,
+                        entity_types,
+                    );
+                    if min_to > 0.0 {
+                        let params = &mut result[group_idx][edge_idx];
+                        params.to_protrusion = params.to_protrusion.max(min_to);
                     }
                 }
             }
